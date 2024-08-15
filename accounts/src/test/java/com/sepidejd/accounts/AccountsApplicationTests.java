3de16ff061f3f357ccad7dbb73dbc719bd7e2632
Sepide Jamshididana <s86908@bht-berlin.de>
2024-08-15 11:56:26 +0200

    first commit

---
+++ accounts/src/test/java/com/sepidejd/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sepidejd.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
